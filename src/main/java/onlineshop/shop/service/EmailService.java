d550ceabb30c474db3a68de8ea36789d267d4459
AK <feniks.42@yandex.ru>
2024-01-05 12:55:28 +0700

    10 Commit

---
+++ src/main/java/onlineshop/shop/service/EmailService.java
@@ -0,0 +1,32 @@
+package onlineshop.shop.service;
+
+import onlineshop.shop.model.Order;
+import onlineshop.shop.model.User;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    private final JavaMailSender javaMailSender;
+
+    public EmailService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    public void sendOrderConfirmation(String recipientEmail, Order order) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(recipientEmail);
+        message.setSubject("Подтверждение заказа");
+        message.setText("Заказ #" + order.getId() + " успешно размещен.");
+        javaMailSender.send(message);
+    }
+    public void sendSuccessRegistration(User user){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(user.getEmail());
+        message.setSubject("Вы успешно зарегестрированы на website");
+        message.setText("Добро пожаловать, " + user.getName());
+        javaMailSender.send(message);
+    }
+}
