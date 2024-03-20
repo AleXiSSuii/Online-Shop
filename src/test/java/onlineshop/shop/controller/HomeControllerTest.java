237f2750fab346c78bed55d0459d000b45a5ef6c
AK <feniks.42@yandex.ru>
2024-03-20 16:35:02 +0700

    new Tests

--- src/test/java/onlineshop/shop/controller/HomeControllerTest.java
+++ src/test/java/onlineshop/shop/controller/HomeControllerTest.java
@@ -11,7 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
+
 
 import java.util.Optional;
 
