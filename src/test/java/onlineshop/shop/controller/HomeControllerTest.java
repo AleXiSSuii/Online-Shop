f6077f122d4d3b643f53931025826873474a64da
AK <feniks.42@yandex.ru>
2024-03-21 15:58:10 +0700

    Commit

--- src/test/java/onlineshop/shop/controller/HomeControllerTest.java
+++ src/test/java/onlineshop/shop/controller/HomeControllerTest.java
@@ -5,7 +5,6 @@ import onlineshop.shop.model.Product;
 import onlineshop.shop.repository.ProductRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
